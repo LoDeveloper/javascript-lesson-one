c807ec75ad9b7e183ec19897919787d7b19451c5
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2024-10-15 10:57:34 +0200

    print console and variable

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let x = 'Hello world!';
+console.log(x);
+
+let firstName = "lorena";
+let age = "35";
+let favoriteColor = "purple";
+
+// Use backticks for template literals
+console.log(`my name is ${firstName}`);
+console.log(`my age is ${age}`);
+console.log(`favorite color is ${favoriteColor}`);
+

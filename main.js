27b80461319161d05baecb6ed0665b932a1bf199
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2024-10-15 11:04:25 +0200

    basic operations

--- main.js
+++ main.js
@@ -6,8 +6,18 @@ let firstName = "lorena";
 let age = "35";
 let favoriteColor = "purple";
 
-// Use backticks for template literals
+
 console.log(`my name is ${firstName}`);
 console.log(`my age is ${age}`);
 console.log(`favorite color is ${favoriteColor}`);
 
+
+
+let myPets = 12;
+let numberOfChairs = 54;
+
+myPets += 8;
+numberOfChairs /=2;
+
+console.log(myPets);
+console.log(numberOfChairs);
